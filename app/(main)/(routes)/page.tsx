ab218546fea4aa36826164750ab8e422b324b3d0
vkayy <vkrice2475@gmail.com>
2023-12-16 01:31:28 +0000

    feat: add clerk authentication

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,6 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div>
+      <UserButton afterSignOutUrl="/"/>
       this is a protected route.
     </div>
   );
