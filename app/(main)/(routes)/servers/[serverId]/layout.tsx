1e95589ac81932cffd9201fa0c77d556e963ce99
vkayy <vkrice2475@gmail.com>
2023-12-17 21:08:52 +0000

    feat: add server sidebar dropdown

---
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -0,0 +1,44 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { ServerSiderbar } from "@/components/server/server-sidebar";
+
+const ServerIdLayout = async ({
+	children,
+	params,
+}: {
+	children: React.ReactNode;
+	params: { serverId: string };
+}) => {
+	const profile = await currentProfile();
+
+	if (!profile) return redirectToSignIn();
+
+	const server = await db.server.findUnique({
+		where: {
+			id: params.serverId,
+			members: {
+				some: {
+					profileId: profile.id,
+				},
+			},
+		},
+	});
+
+	if (!server) redirect("/");
+
+	return (
+    <div className="h-full">
+      <div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
+        <ServerSiderbar serverId={params.serverId} />
+      </div>
+      <main className="h-full md:pl-60">
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default ServerIdLayout;
