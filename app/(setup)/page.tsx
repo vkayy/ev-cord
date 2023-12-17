c17ca5049a0f2a703cb06eda5cbed9708d84620e
vkayy <vkrice2475@gmail.com>
2023-12-17 04:08:01 +0000

    feat: add file upload and server name entry

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,6 +1,7 @@
 import { redirect } from "next/navigation";
 import { db } from "@/lib/db";
 import { initialProfile } from "@/lib/initial-profile";
+import InitialModal from "@/components/modals/initial-modal";
 
 const SetupPage = async () => {
     const profile = await initialProfile();
@@ -16,7 +17,7 @@ const SetupPage = async () => {
 
     if (server) return redirect(`/servers/${server.id}`)
 
-    return <div>create a server!</div>
+    return <InitialModal />
 }
  
 export default SetupPage;
