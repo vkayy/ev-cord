97b2e3141acfe0d86578167d0109cafe4757c88b
vkayy <vkrice2475@gmail.com>
2023-12-21 21:40:18 +0000

    feat: add basic message rendering

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,5 +1,6 @@
 import { ChatHeader } from "@/components/chat/chat-header";
 import { ChatInput } from "@/components/chat/chat-input";
+import { ChatMessages } from "@/components/chat/chat-messages";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
@@ -39,7 +40,22 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
 				serverId={channel.serverId}
 				type="channel"
 			/>
-			<div className="flex-1">future messages</div>
+			<div className="flex-1">
+				<ChatMessages
+					name={channel.name}
+					member={member}
+					chatId={channel.id}
+					apiUrl="/api/messages"
+					socketUrl="/api/socket/messages"
+					socketQuery={{
+						channelId: channel.id,
+						serverId: channel.serverId,
+					}}
+          paramKey="channelId"
+          paramValue={channel.id}
+          type="channel"
+				/>
+			</div>
 			<ChatInput
 				apiUrl="/api/socket/messages"
 				query={{
--- types.ts
+++ types.ts
@@ -1,7 +1,7 @@
 import { Server as NetServer, Socket } from "net";
 import { NextApiResponse } from "next";
 import { Server as SocketIOServer } from "socket.io";
-import { Server, Member, Profile } from '@prisma/client'
+import { Server, Member, Profile, Message } from '@prisma/client'
 
 export type ServerWithMembersWithProfiles = Server & {
   members: (Member & { profile: Profile } )[]
@@ -18,5 +18,11 @@ export type NextApiResponseServerIo = NextApiResponse & {
 export type SocketWithServer = Socket & {
   server: NetServer & {
     io: SocketIOServer
+  }
+}
+
+export type MessageWithMemberWithProfile = Message & {
+  member: Member & {
+    profile: Profile
   }
 }
