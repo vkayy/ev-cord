77c4cfc652a8bc432a32f029372c05f9264595c4
vkayy <vkrice2475@gmail.com>
2023-12-21 13:20:09 +0000

    feat: add conversation page

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,30 +1,33 @@
-import { Hash } from "lucide-react"
+import { Hash } from "lucide-react";
 import { UserAvatar } from "@/components/user-avatar";
 import { MobileToggle } from "@/components/mobile-toggle";
 
 interface ChatHeaderProps {
-  serverId: string;
-  name: string;
-  type: "channel" | "conversation";
-  imageUrl?: string;
+	serverId: string;
+	name: string;
+	type: "channel" | "conversation";
+	imageUrl?: string;
 }
 
 export const ChatHeader = ({
-  serverId,
-  name,
-  type,
-  imageUrl,
+	serverId,
+	name,
+	type,
+	imageUrl,
 }: ChatHeaderProps) => {
-  return (
-    <div className="text-md font-semibold px-3 flex items-center h-12
-    border-neutral-200 dark:border-neutral-800 border-b-2">
-      <MobileToggle serverId={serverId} />
-      {type === "channel" && (
-        <Hash className="h-5 w-5 ml-2 text-zinc-500 dark:text-zinc-400 mr-2" />
-      )}
-      <p className="font-semibold text-md text-black dark:text-white">
-        {name}
-      </p>
-    </div>
-  )
-}
+	return (
+		<div
+			className="text-md font-semibold px-3 flex items-center h-12
+    border-neutral-200 dark:border-neutral-800 border-b-2"
+		>
+			<MobileToggle serverId={serverId} />
+			{type === "channel" && (
+				<Hash className="h-5 w-5 ml-2 text-zinc-500 dark:text-zinc-400 mr-2" />
+			)}
+			{type === "conversation" && (
+				<UserAvatar src={imageUrl} className="h-8 w-8 md:h-8 md:w-8 mr-2" />
+			)}
+			<p className="font-semibold text-md text-black dark:text-white">{name}</p>
+		</div>
+	);
+};
